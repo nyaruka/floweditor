4878774ff59914e64072e5c546b8d390ef47c518
Eric Newcomer <ericnewcomer@gmail.com>
2017-05-31 09:37:05 -0500

    wipeout bad flows

--- src/components/FlowLoader.tsx
+++ src/components/FlowLoader.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import * as UUID from 'uuid';
 import * as update from 'immutability-helper';
 
-import {Temba} from '../services/Temba';
-import {Node} from './Node';
-import {Plumber} from '../services/Plumber';
-import {FlowStore} from '../services/FlowStore';
-import {Simulator} from './Simulator';
-import {Config} from '../services/Config';
-import {NodeModal} from './NodeModal';
-import {FlowMutator} from './FlowMutator';
-import {Flow} from './Flow';
-import {FlowDefinition, Endpoints} from '../interfaces';
-import {External} from '../services/External';
+import { Temba } from '../services/Temba';
+import { Node } from './Node';
+import { Plumber } from '../services/Plumber';
+import { FlowStore } from '../services/FlowStore';
+import { Simulator } from './Simulator';
+import { Config } from '../services/Config';
+import { NodeModal } from './NodeModal';
+import { FlowMutator } from './FlowMutator';
+import { Flow } from './Flow';
+import { FlowDefinition, Endpoints } from '../interfaces';
+import { External } from '../services/External';
 
 var FORCE_FETCH = true;
 var QUIET_UI = 10;
@@ -50,25 +50,38 @@ export class FlowLoader extends React.PureComponent<FlowLoaderProps, FlowLoaderS
         }
     }
 
+    private initialize(definition: FlowDefinition) {
+        this.mutator = new FlowMutator(
+            definition,
+            this.setDefinition.bind(this),
+            this.save.bind(this),
+            this.props,
+            QUIET_UI, QUIET_SAVE
+        );
+        this.setDefinition(definition);
+    }
+
     componentDidMount() {
-        this.props.external.getFlow(this.props.uuid).then((definition: FlowDefinition) =>{
-            this.mutator = new FlowMutator(
-                definition,
-                this.setDefinition.bind(this), 
-                this.save.bind(this), 
-                this.props,
-                QUIET_UI, QUIET_SAVE
-            );
-            this.setDefinition(definition);
+        this.props.external.getFlow(this.props.uuid).then((definition: FlowDefinition) => {
+
+            try {
+                this.initialize(definition);
+            }
+
+            // if we fail to initialize, wipe out our nodes and reset
+            catch (e) {
+                definition = update(definition, { $merge: { nodes: [], _ui: { nodes: {} } } });
+                this.initialize(definition);
+            }
         });
     }
 
     private setDefinition(definition: FlowDefinition, dependencies?: FlowDefinition[]) {
         // TODO: determine full dependency list and fetch those at simulation time
         if (!dependencies) {
-            this.setState({definition: definition});
+            this.setState({ definition: definition });
         } else {
-            this.setState({definition: definition, dependencies: dependencies});
+            this.setState({ definition: definition, dependencies: dependencies });
         }
     }
 
@@ -76,10 +89,10 @@ export class FlowLoader extends React.PureComponent<FlowLoaderProps, FlowLoaderS
         var flow = null;
         if (this.state.definition) {
             flow = <Flow
-                    endpoints={this.props.endpoints}
-                    definition={this.state.definition}
-                    dependencies={this.state.dependencies}
-                    mutator={this.mutator}/>
+                endpoints={this.props.endpoints}
+                definition={this.state.definition}
+                dependencies={this.state.dependencies}
+                mutator={this.mutator} />
         }
         return flow;
     }
