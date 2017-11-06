06497b6de1f7aa0612514bab27f4fb013f9f3a8a
Kellan Alexander <ycleptkellan@gmail.com>
2017-11-06 07:00:52 -0800

    initial test: Flow

---
+++ src/components/Flow.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import '../enzymeAdapter';
+import { shallow } from 'enzyme';
+import Flow, { IFlowProps } from '../components/Flow';
+import EditorConfig from '../services/EditorConfig';
+import External from '../services/External';
+import FlowMutator from '../services/FlowMutator';
+import CompMap from '../services/ComponentMap';
+
+const {
+    results: [{ definition }]
+} = require('../../test_flows/a4f64f1b-85bc-477e-b706-de313a022979.json');
+
+const ComponentMap = new CompMap(definition);
+
+const Mutator = new FlowMutator(ComponentMap, definition, jest.fn(), jest.fn());
+
+const flowProps: IFlowProps = {
+    EditorConfig: new EditorConfig(),
+    External: new External(),
+    definition,
+    dependencies: null,
+    Mutator,
+    ComponentMap
+};
+
+const FlowShallow = shallow(<Flow {...flowProps} />);
+
+describe('Component: Flow', () => {
+    it('should render', () => {
+        expect(FlowShallow).toBePresent();
+    });
+});
