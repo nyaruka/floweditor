753bf1a51c82b5dee53ae89b5c0a34b9363e893e
Kellan Alexander <ycleptkellan@gmail.com>
2017-11-06 06:39:39 -0800

    remove FlowLoader, refactor Editor/Editor tests

--- src/components/Editor.test.tsx
+++ src/components/Editor.test.tsx
@@ -1,30 +1,42 @@
 import * as React from 'react';
-import '../../enzyme.adapter';
-import { shallow } from 'enzyme';
-import { Editor } from './Editor';
-import { FlowLoader } from './FlowLoader';
-import { editorConfig } from '../../__tests__/flow-editor-config'; 
-import { getSpecWrapper, validUUID } from '../../__tests__/utils';
+import '../enzymeAdapter';
+import { shallow, mount } from 'enzyme';
+import Editor, { IEditorProps } from './Editor';
+import Flow from './Flow';
+import { flowUUID } from '../flowEditorConfig';
+import EditorConfig from '../services/EditorConfig';
+import External from '../services/External';
+import { getSpecWrapper } from '../helpers/utils';
 
-const EditorShallow = shallow(<Editor config={editorConfig} />);
+const {
+    results: [{ definition }]
+} = require('../../test_flows/a4f64f1b-85bc-477e-b706-de313a022979.json');
 
-describe('Editor Component', () => {
-    it('Initializes with expected state', () => {
-        const flowUUID: string = editorConfig.flow; 
+const editorProps: IEditorProps = {
+    flowUUID,
+    EditorConfig: new EditorConfig(),
+    External: {
+        getFlow: jest.fn(() =>
+            new Promise((resolve, reject) =>
+                process.nextTick(
+                    () => resolve({
+                        definition
+                    })
+                )
+            )
+        )
+    } as any
+};
+
+const EditorShallow = shallow(<Editor {...editorProps} />);
+
+describe('Component: Editor', () => {
+    it('Renders w/ expected state', () => {
+        const { EditorConfig: { endpoints: { flows: flowsEndpoint } }, External: { getFlow } } = editorProps;
         expect(EditorShallow).toBePresent();
+        expect(getSpecWrapper(EditorShallow, 'editor')).toBePresent();
         expect(EditorShallow).toHaveState('flowUUID', flowUUID);
-    });
-
-    it('Renders the editor div', () => {
-        const editorDivShallow = getSpecWrapper(EditorShallow, 'editor');
-        expect(editorDivShallow).toBePresent();
-    });
-
-    it('Renders a FlowLoader component with valid key and props', () => {
-        const FlowLoaderShallow = EditorShallow.find(FlowLoader);
-        expect(FlowLoaderShallow).toBePresent();
-        expect(validUUID(FlowLoaderShallow.key())).toBeTruthy();
-        expect(FlowLoaderShallow).toHaveProp('uuid');
-        expect(validUUID(FlowLoaderShallow.prop('uuid'))).toBeTruthy();
+        expect(getFlow).toBeCalledWith(flowUUID, false, flowsEndpoint);
+        expect(EditorShallow).toHaveState('definition', definition);
     });
 });
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,53 +1,121 @@
 import * as React from 'react';
 import * as axios from 'axios';
 import * as UUID from 'uuid';
-import { External, FlowDetails } from '../services/External';
-import { FlowLoader } from './FlowLoader';
 // import { FlowList } from './FlowList';
-import { Endpoints, Config } from '../services/Config';
+import EditorConfig from '../services/EditorConfig';
+import External, { IFlowDetails } from '../services/External';
+import { IFlowDefinition } from '../flowTypes';
+import FlowMutator from '../services/FlowMutator';
+import { Temba } from '../services/Temba';
+import ComponentMap from '../services/ComponentMap';
+import Flow from './Flow';
 
 const styles = require('./Editor.scss');
 
-interface EditorProps {
-    config: FlowEditorConfig;
+export interface IEditorProps {
+    flowUUID: string;
+    EditorConfig: EditorConfig;
+    External: External;
 }
 
-interface EditorState {
-    flowUUID?: string;
+export interface IEditorState {
+    flowUUID: string;
+    definition: IFlowDefinition;
+    dependencies: IFlowDefinition[];
 }
 
 /**
  * A navigable list of flows for an account
  */
-export class Editor extends React.PureComponent<EditorProps, EditorState> {
-    private external: External;    
+class Editor extends React.PureComponent<IEditorProps, IEditorState> {
+    private Temba: Temba;
+    private Mutator: FlowMutator;
+    private ComponentMap: ComponentMap;
 
-    constructor(props: EditorProps) {
+    constructor(props: IEditorProps) {
         super(props);
         this.state = {
-            flowUUID: this.props.config.flow
+            flowUUID: this.props.flowUUID,
+            definition: null,
+            dependencies: null
         };
-        Config.initialize(this.props.config);
+
+        // this.Temba = new Temba('https://your-site.com', '05594lM5uQsTHLlvrBts5lenb5Iyex6P');
+
+        this.onFlowSelect = this.onFlowSelect.bind(this);
     }
 
-    private onFlowSelect(uuid: string) {
-        this.setState({ flowUUID: uuid });
+    private onFlowSelect(uuid: string): void {
+        this.setState({ flowUUID: uuid }, () => this.fetchFlow());
     }
 
-    public render() {
-        let flow;
-
-        if (this.state.flowUUID) {
-            flow = <FlowLoader key={this.state.flowUUID} uuid={this.state.flowUUID} />;
+    private setDefinition(definition: IFlowDefinition, dependencies?: IFlowDefinition[]): void {
+        if (dependencies) {
+            this.setState({
+                definition,
+                dependencies
+            });
         } else {
-            const newUUID = UUID.v4();
-            flow = <FlowLoader key={newUUID} uuid={newUUID} />;
+            this.setState({
+                definition
+            });
         }
-        // disable the flow list for now
-        // var flowList = (<FlowList
-        // temba={this.temba}
-        //    onFlowSelect={this.onFlowSelect.bind(this)}
-        /// >)
-        return <div className={styles.editor} data-spec='editor'>{flow}</div>;
-    }        
+    }
+
+    private save(definition: IFlowDefinition) {
+        // this.props.External.saveFlow(definition).catch((error) => {
+        // do nothing
+        //});
+    }
+
+    /** TODO: determine full dependency list and fetch those at simulation time */
+    private initialize(definition: IFlowDefinition): void {
+        this.ComponentMap = new ComponentMap(definition);
+
+        this.Mutator = new FlowMutator(
+            this.ComponentMap,
+            definition,
+            this.setDefinition.bind(this),
+            this.save.bind(this)
+            // this.props,
+        );
+
+        this.setDefinition(definition);
+    }
+
+    private fetchFlow(): void {
+        this.props.External
+            .getFlow(this.state.flowUUID, false, this.props.EditorConfig.endpoints.flows)
+            .then(({ definition }: IFlowDetails) => this.initialize(definition))
+            .catch((error: {}) => console.log(error));
+    }
+
+    public componentDidMount(): void {
+        this.fetchFlow();
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div className={styles.editor} data-spec="editor">
+                {/* disable the flow list for now
+                    <FlowList
+                        getFlows={this.temba.getFlows}
+                        onFlowSelect={this.onFlowSelect.bind(this)}
+                    />
+                */}
+                {this.state.definition ? (
+                    <Flow
+                        EditorConfig={this.props.EditorConfig}
+                        External={this.props.External}
+                        definition={this.state.definition}
+                        dependencies={this.state.dependencies}
+                        ComponentMap={this.ComponentMap}
+                        Mutator={this.Mutator}
+                    />
+                ) : null}
+            </div>
+        );
+    }
 }
+
+export default Editor;
