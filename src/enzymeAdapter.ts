93f3a7b569b88fc5e40d63cd4b118a1ae62532ab
Kellan Alexander <ycleptkellan@gmail.com>
2017-11-07 00:33:33 -0800

    fix build

--- src/enzymeAdapter.ts
+++ src/enzymeAdapter.ts
@@ -1,4 +1,4 @@
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
