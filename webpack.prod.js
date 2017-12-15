f0f4eb23ef1c6296f2079220d1319cbd64e36c45
Kellan Alexander <ycleptkellan@gmail.com>
2017-12-15 14:04:52 -0800

    favor babel-minify-webpack-plugin over uglifyjs plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const CompressionPlugin = require('compression-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 const { smartStrategy } = require('webpack-merge');
 const { LoaderOptionsPlugin, DefinePlugin } = require('webpack');
 const commonConfig = require('./webpack.common');
@@ -20,22 +20,7 @@ const prodConfig = {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
-        new UglifyJSPlugin({
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                warnings: false /** Suppress uglification warnings */,
-                screw_ie8: true,
-                drop_console: true
-            },
-            output: {
-                beautify: false,
-                comments: false
-            },
-            exclude: [/\.min\.(j|t)sx?$/gi] /** Skip pre-minified libs */
-        }),
+        new MinifyPlugin(),
         new CompressionPlugin({
             asset: '[path].gz[query]',
             algorithm: 'gzip',
