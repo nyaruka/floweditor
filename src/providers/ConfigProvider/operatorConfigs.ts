08e846aaa090706e12396548a9fe74592926de79
Kellan Alexander <ycleptkellan@Kellans-MacBook-Pro.local>
2018-01-08 16:16:11 -0800

    reorder operators

--- src/providers/ConfigProvider/operatorConfigs.ts
+++ src/providers/ConfigProvider/operatorConfigs.ts
@@ -17,6 +17,18 @@ export const operatorConfigList: Operator[] = [
     { type: 'has_only_phrase', verboseName: 'has only the phrase', operands: 1 },
     { type: 'has_beginning', verboseName: 'starts with', operands: 1 },
     {
+        type: 'has_text',
+        verboseName: 'has some text',
+        operands: 0,
+        categoryName: 'Has Text'
+    },
+    {
+        type: 'has_number',
+        verboseName: 'has a number',
+        operands: 0,
+        categoryName: 'Has Number'
+    },
+    {
         type: 'has_number_between',
         verboseName: 'has a number between',
         operands: 2
@@ -34,24 +46,12 @@ export const operatorConfigList: Operator[] = [
         operands: 1
     },
     { type: 'has_number_gt', verboseName: 'has a number above', operands: 1 },
+    { type: 'has_date', verboseName: 'has a date', operands: 0, categoryName: 'Has Date' },
     { type: 'has_date_lt', verboseName: 'has a date before', operands: 1 },
     { type: 'has_date_eq', verboseName: 'has a date equal to', operands: 1 },
     { type: 'has_date_gt', verboseName: 'has a date after', operands: 1 },
     { type: 'has_run_status', verboseName: 'has a run status of', operands: 1 },
     { type: 'has_group', verboseName: 'is in the group', operands: 1 },
-    { type: 'has_date', verboseName: 'has a date', operands: 0, categoryName: 'Has Date' },
-    {
-        type: 'has_text',
-        verboseName: 'has some text',
-        operands: 0,
-        categoryName: 'Has Text'
-    },
-    {
-        type: 'has_number',
-        verboseName: 'has a number',
-        operands: 0,
-        categoryName: 'Has Number'
-    },
     {
         type: 'has_phone',
         verboseName: 'has a phone number',
