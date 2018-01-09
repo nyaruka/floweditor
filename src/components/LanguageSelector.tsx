cee917320682af9e3e004f89265b711c0e556d71
ycleptkellan <ycleptkellan@gmail.com>
2018-01-08 22:11:33 -0800

    LanguageSelector should be functional

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Select from 'react-select';
 import { Languages } from '../flowTypes';
 
-import * as styles from './LanguageSelector.scss';
+import { languageSelector } from './LanguageSelector.scss';
 
 export interface Language {
     name: string;
@@ -15,36 +15,27 @@ export interface LanguageSelectorProps {
     onChange(language: Language): void;
 }
 
-export default class LanguageSelectorComp extends React.PureComponent<LanguageSelectorProps, {}> {
-    private options: Language[] = [];
+const LanguageSelectorComp: React.SFC<LanguageSelectorProps> = props => {
+    const options: Language[] = Object.keys(props.languages).map(iso => ({
+        name: props.languages[iso],
+        iso
+    }));
 
-    constructor(props: LanguageSelectorProps) {
-        super(props);
+    return (
+        <div className={`${languageSelector} select-small`}>
+            <Select
+                // Flow
+                value={props.iso}
+                onChange={props.onChange}
+                // LanguageSelector
+                valueKey="iso"
+                labelKey="name"
+                searchable={false}
+                clearable={false}
+                options={options}
+            />
+        </div>
+    );
+};
 
-        this.options = Object.keys(this.props.languages).map(iso => {
-            const name = this.props.languages[iso];
-            return {
-                name,
-                iso
-            };
-        });
-    }
-
-    render() {
-        return (
-            <div className={`${styles.ele} select-small`}>
-                <Select
-                    /** Flow */
-                    value={this.props.iso}
-                    onChange={this.props.onChange}
-                    /** LanguageSelector */
-                    valueKey="iso"
-                    labelKey="name"
-                    searchable={false}
-                    clearable={false}
-                    options={this.options}
-                />
-            </div>
-        );
-    }
-}
+export default LanguageSelectorComp;
