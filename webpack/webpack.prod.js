0a535f013672dfe25abb64ff716fdeb41b124944
ycleptkellan <ycleptkellan@gmail.com>
2018-03-02 15:44:18 -0800

    hot load scss globals

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,9 @@
 const { optimize: { ModuleConcatenationPlugin } } = require('webpack');
 const { smartStrategy } = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const paths = require('./paths');
 const { uglifyPlugin, compressionPlugin } = require('./plugins');
+const { typingsForCssModulesLoader, postCSSLoader } = require('./loaders');
 const commonConfig = require('./webpack.common');
 
 const prodConfig = {
@@ -10,10 +12,41 @@ const prodConfig = {
         path: paths.distProd,
         publicPath: '/'
     },
-    plugins: [new ModuleConcatenationPlugin(), uglifyPlugin, compressionPlugin]
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new ModuleConcatenationPlugin(),
+        uglifyPlugin,
+        compressionPlugin
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                include: [paths.component],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        typingsForCssModulesLoader(true),
+                        postCSSLoader,
+                        'sass-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.s?css$/,
+                include: [paths.lib],
+                exclude: [paths.component],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', postCSSLoader, 'sass-loader']
+                })
+            }
+        ]
+    }
 };
 
 module.exports = smartStrategy({
     output: 'append',
-    plugins: 'append'
+    plugins: 'append',
+    'module.rules': 'prepend'
 })(commonConfig, prodConfig);
