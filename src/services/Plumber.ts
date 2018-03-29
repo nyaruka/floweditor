d902798283690fe92bd58cd167c9baf813ff336b
ycleptkellan <ycleptkellan@gmail.com>
2018-03-29 15:26:03 -0700

    square w/ master

--- src/services/Plumber.ts
+++ src/services/Plumber.ts
@@ -1,4 +1,4 @@
-const { jsPlumb: { importDefaults } } = require('../../node_modules/jsplumb/dist/js/jsplumb');
+const { jsPlumb: { importDefaults } } = require('../../node_modules/jsplumb/dist/js/jsplumb.min');
 import { Node, FlowDefinition, Exit, LocalizationMap } from '../flowTypes';
 
 export interface DragEvent {
