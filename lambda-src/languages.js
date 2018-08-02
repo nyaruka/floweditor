bbe8c0f231d241c310cdd0c4d1f308b64feaf89f
Eric Newcomer <ericnewcomer@gmail.com>
2018-08-02 14:30:41 -0500

    fix lambda languages

--- lambda-src/languages.js
+++ lambda-src/languages.js
@@ -1,4 +1,5 @@
 const languagesResp = require('../preview/assets/languages.json');
 const { getOpts } = require('./utils');
 
-exports.handler = (evt, ctx, cb) => cb(null, getOpts({ body: JSON.stringify(languagesResp) }));
+exports.handler = (evt, ctx, cb) =>
+    cb(null, getOpts({ body: JSON.stringify(languagesResp.assets) }));
