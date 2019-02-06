b1dec3a8445973c12aca8e25cffe0d7a43d09fad
Eric Newcomer <ericnewcomer@gmail.com>
2019-02-05 16:42:32 -0800

    Support non-RP based endpoints in preview

--- lambda-src/utils/index.js
+++ lambda-src/utils/index.js
@@ -3,6 +3,8 @@ const baseResponse = {
     headers: { 'Content-Type': 'application/json' }
 };
 
+exports.getOpts = (opts = {}) => Object.assign({}, baseOpts, opts);
+
 exports.respond = (callback, body = {}) => {
     callback(null, Object.assign({}, baseResponse, { body: JSON.stringify(body) }));
 };
