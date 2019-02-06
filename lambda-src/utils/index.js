99a073c36274f800f69f7f9170051e555330ccf2
Eric Newcomer <ericnewcomer@gmail.com>
2019-02-05 16:46:46 -0800

    Rename baseOpts

--- lambda-src/utils/index.js
+++ lambda-src/utils/index.js
@@ -3,7 +3,7 @@ const baseResponse = {
     headers: { 'Content-Type': 'application/json' }
 };
 
-exports.getOpts = (opts = {}) => Object.assign({}, baseOpts, opts);
+exports.getOpts = (opts = {}) => Object.assign({}, baseResponse, opts);
 
 exports.respond = (callback, body = {}) => {
     callback(null, Object.assign({}, baseResponse, { body: JSON.stringify(body) }));
