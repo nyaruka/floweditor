a204a8f8799b22b2e0a816aca46b2d97536c0785
Eric Newcomer <ericnewcomer@gmail.com>
2019-05-17 16:56:39 -0500

    Use base netlify endpoints in preview and update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,13 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    // we serve netlify functions locally in dev
+    // % yarn run lambda:build && yarn run lamba:serve
     app.use(
         proxy('/.netlify/functions/', {
             target: 'http://localhost:6000/',
             pathRewrite: {
                 '^/\\.netlify/functions': ''
-            }
-        })
-    );
-
-    app.use(
-        proxy('/assets/', {
-            target: 'http://localhost:6000/',
-            pathRewrite: {
-                '^/assets': ''
             }
         })
     );
