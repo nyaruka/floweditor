1090f1e177bbd55ac612ff6620c9b7f30593ac59
Eric Newcomer <ericnewcomer@gmail.com>
2019-05-20 11:53:22 -0500

    Change default name for preview

--- lambda/revisions.js
+++ lambda/revisions.js
@@ -1,57 +1,57 @@
 import { respond } from './utils/index.js';
 
+const user = {
+  email: 'chancerton@nyaruka.com',
+  name: 'Chancellor von Frankenbean'
+};
+
 const assetList = [
-    {
-        user: {
-            email: 'msburgerbuns@nyaruka.com',
-            name: 'Leah Burgerbuns'
-        },
-        created_on: new Date(),
-        id: 1,
-        version: '13',
-        revision: 1
-    }
+  {
+    user: user,
+    created_on: new Date(),
+    id: 1,
+    version: '13',
+    revision: 1
+  }
 ];
+
 const assetContent = {
-    1: {
-        name: 'Favorites',
-        language: 'eng',
-        type: 'message',
-        uuid: 'a4f64f1b-85bc-477e-b706-de313a022979',
-        localization: {},
-        nodes: [],
-        _ui: {
-            languages: [{ eng: 'English' }, { spa: 'Spanish' }]
-        }
+  1: {
+    name: 'Favorites',
+    language: 'eng',
+    type: 'message',
+    uuid: 'a4f64f1b-85bc-477e-b706-de313a022979',
+    localization: {},
+    nodes: [],
+    _ui: {
+      languages: [{ eng: 'English' }, { spa: 'Spanish' }]
     }
+  }
 };
 
 exports.handler = (request, context, callback) => {
-    if (request.httpMethod === 'POST') {
-        const id = Object.keys(assetContent).length + 1;
-        assetContent[id] = request.body;
+  if (request.httpMethod === 'POST') {
+    const id = Object.keys(assetContent).length + 1;
+    assetContent[id] = request.body;
 
-        const asset = {
-            user: {
-                email: 'msburgerbuns@nyaruka.com',
-                name: 'Leah Burgerbuns'
-            },
-            created_on: new Date(),
-            id,
-            version: '13',
-            revision: id
-        };
-        assetList.unshift(asset);
-        respond(callback, asset);
-        return;
-    }
+    const asset = {
+      user: user,
+      created_on: new Date(),
+      id,
+      version: '13',
+      revision: id
+    };
+    assetList.unshift(asset);
+    respond(callback, asset);
+    return;
+  }
 
-    const regex = /.*\/revisions\/(\d+)/;
-    const match = regex.exec(request.path);
+  const regex = /.*\/revisions\/(\d+)/;
+  const match = regex.exec(request.path);
 
-    if (match && match.length > 1) {
-        respond(callback, assetContent[match[1]]);
-    } else {
-        respond(callback, { results: assetList });
-    }
+  if (match && match.length > 1) {
+    respond(callback, assetContent[match[1]]);
+  } else {
+    respond(callback, { results: assetList });
+  }
 };
