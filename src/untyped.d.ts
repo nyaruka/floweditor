c6afb7ca807ca6fad8477e133c046f94e4b20b74
Eric Newcomer <ericnewcomer@gmail.com>
2019-06-18 13:09:41 -0700

    Add RFC-based header name validation

--- src/untyped.d.ts
+++ src/untyped.d.ts
@@ -2,3 +2,4 @@ declare module 'split-sms';
 declare module 'get-input-selection';
 declare module 'classnames/bind';
 declare module 'http-proxy-middleware';
+declare module 'http-headers-validation';
