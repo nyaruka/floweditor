5ff5f4b09789075283af40a9cc45b3177a777b78
Eric Newcomer <ericnewcomer@gmail.com>
2019-10-04 13:09:28 -0700

    Allow result type to trump type request

--- src/components/form/assetselector/AssetSelector.test.tsx
+++ src/components/form/assetselector/AssetSelector.test.tsx
@@ -1,18 +1,30 @@
 import AssetSelector from 'components/form/assetselector/AssetSelector';
 import React from 'react';
 import { Assets, AssetType } from 'store/flowContext';
-import { render } from 'test/utils';
+import { render, fireEvent } from 'test/utils';
+import { createUUID } from 'utils';
+import { mock } from 'testUtils';
+import * as utils from 'utils';
+
+const group_uuid = createUUID();
+mock(utils, 'createUUID', utils.seededUUIDs());
 
 const assets: Assets = {
-  items: {},
-  type: AssetType.Group
+  items: {
+    [group_uuid]: {
+      id: group_uuid,
+      name: 'My Test Group',
+      type: AssetType.Group
+    }
+  },
+  type: AssetType.Contact || AssetType.Group
 };
 
 describe(AssetSelector.name, () => {
   it('renders', () => {
     const { baseElement } = render(
       <AssetSelector
-        name="Groups"
+        name="Recipients"
         onChange={jest.fn()}
         assets={assets}
         createAssetFromInput={jest.fn()}
