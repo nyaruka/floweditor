33dcbeb24b37607b7dbe4e3b96a21484c28e8fee
pankaj-ag <pankaj@coloredcow.in>
2020-09-15 17:54:48 +0530

    with build

---
+++ build/precache-manifest.2d0841999aae460d4df4d3b9ea432f72.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '15a0d3c5569cc6983277d7244c71a2ca',
+    url: './index.html'
+  },
+  {
+    revision: 'b9ef6b23de5b200aa2ce',
+    url: './static/css/main.7c1dfe12.chunk.css'
+  },
+  {
+    revision: '88b332c0b4401aaa9b38',
+    url: './static/js/2.b71e2dbc.chunk.js'
+  },
+  {
+    revision: 'fbf73d1ed7e41d48ebcb53ec4d810884',
+    url: './static/js/2.b71e2dbc.chunk.js.LICENSE.txt'
+  },
+  {
+    revision: 'c9eca76251fc5768b6cb',
+    url: './static/js/3.52130224.chunk.js'
+  },
+  {
+    revision: 'b9ef6b23de5b200aa2ce',
+    url: './static/js/main.8b609150.chunk.js'
+  },
+  {
+    revision: '75743f3ed38f0689098f',
+    url: './static/js/runtime-main.4af094c1.js'
+  },
+  {
+    revision: '9e6245520bfe139052f69101e02296b1',
+    url: './static/media/floweditor.9e624552.woff'
+  },
+  {
+    revision: 'cd6a1374dab8144ad05b8996c2bde8d6',
+    url: './static/media/floweditor.cd6a1374.ttf'
+  },
+  {
+    revision: 'f32c1977cdc4269929df2fc475b85cba',
+    url: './static/media/floweditor.f32c1977.svg'
+  },
+  {
+    revision: '5e547148e6fcbe72b9949c4597566831',
+    url: './static/media/map.5e547148.jpg'
+  }
+]);
