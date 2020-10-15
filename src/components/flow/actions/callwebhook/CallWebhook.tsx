daef92fe88609980159066c853aa9bdf536116f9
Eric Newcomer <ericnewcomer@gmail.com>
2020-10-14 17:43:00 -0700

    Truncate save result and webhook text on main edit view

--- src/components/flow/actions/callwebhook/CallWebhook.tsx
+++ src/components/flow/actions/callwebhook/CallWebhook.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { CallWebhook } from 'flowTypes';
+import { ellipsize } from 'utils';
 
-const CallWebhookComp: React.SFC<CallWebhook> = ({ url }): JSX.Element => <div>{url}</div>;
+const CallWebhookComp: React.SFC<CallWebhook> = ({ url }): JSX.Element => (
+  <div>{ellipsize(url, 150)}</div>
+);
 
 export default CallWebhookComp;
