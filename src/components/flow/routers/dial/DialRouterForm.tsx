4fddb11d2021a6639d436d6b7ceac4c792d68142
susanm74 <susan.m.lister@gmail.com>
2022-10-13 17:03:37 +0700

    initial commit with advanced tab

--- src/components/flow/routers/dial/DialRouterForm.tsx
+++ src/components/flow/routers/dial/DialRouterForm.tsx
@@ -1,6 +1,6 @@
 import { react as bindCallbacks } from 'auto-bind';
 import * as React from 'react';
-import Dialog, { ButtonSet } from 'components/dialog/Dialog';
+import Dialog, { ButtonSet, Tab } from 'components/dialog/Dialog';
 import { hasErrors, renderIssues } from 'components/flow/actions/helpers';
 import { RouterFormProps } from 'components/flow/props';
 import { nodeToState, stateToNode } from 'components/flow/routers/dial/helpers';
@@ -10,6 +10,7 @@ import TypeList from 'components/nodeeditor/TypeList';
 import { FormState, StringEntry } from 'store/nodeEditor';
 import { Alphanumeric, Required, StartIsNonNumeric, validate } from 'store/validators';
 import i18n from 'config/i18n';
+import CheckboxElement from 'components/form/checkbox/CheckboxElement';
 
 export interface DialRouterFormState extends FormState {
   phone: StringEntry;
@@ -66,8 +67,33 @@ export default class DialRouterForm extends React.Component<RouterFormProps, Dia
   public renderEdit(): JSX.Element {
     const typeConfig = this.props.typeConfig;
 
+    const advanced: Tab = {
+      name: i18n.t('forms.advanced', 'Advanced'),
+      body: (
+        <CheckboxElement
+          name={i18n.t('forms.all_destinations', 'All Destinations')}
+          title={i18n.t('forms.all_destinations', 'All Destinations')}
+          // todo labelClassName={styles.checkbox}
+          checked={false} // todo checked={this.state.sendAll}
+          description={i18n.t(
+            'forms.all_destinations_description',
+            "Send a message to all destinations known for this contact. If you aren't sure what this means, leave it unchecked."
+          )}
+          // todo onChange={this.handleSendAllUpdate}
+        />
+      )
+      // todo checked: this.state.sendAll
+    };
+
+    const tabs = [advanced];
+
     return (
-      <Dialog title={typeConfig.name} headerClass={typeConfig.type} buttons={this.getButtons()}>
+      <Dialog
+        title={typeConfig.name}
+        headerClass={typeConfig.type}
+        buttons={this.getButtons()}
+        tabs={tabs}
+      >
         <TypeList __className="" initialType={typeConfig} onChange={this.props.onTypeChange} />
         <p>Enter the phone number to dial</p>
         <TextInputElement
