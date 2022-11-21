f9aa8af4979ec6bab9c19a1472243e2ee888f909
susanm74 <susan.m.lister@gmail.com>
2022-11-21 11:32:02 +0700

    only excluding expired exit for the ivr/voice flow type

--- src/components/flow/routers/subflow/helpers.ts
+++ src/components/flow/routers/subflow/helpers.ts
@@ -1,7 +1,7 @@
 import { createRenderNode } from 'components/flow/routers/helpers';
 import { SubflowRouterFormState } from 'components/flow/routers/subflow/SubflowRouterForm';
 import { SUBFLOW_OPERAND } from 'components/nodeeditor/constants';
-import { Operators, Types } from 'config/interfaces';
+import { FlowTypes, Operators, Types } from 'config/interfaces';
 import { getType } from 'config/typeConfigs';
 import {
   Case,
@@ -80,7 +80,6 @@ export const stateToNode = (
         destination_uuid: null
       }
     ];
-
     categories = [
       {
         uuid: createUUID(),
@@ -88,7 +87,6 @@ export const stateToNode = (
         exit_uuid: exits[0].uuid
       }
     ];
-
     cases = [
       {
         uuid: createUUID(),
@@ -97,6 +95,25 @@ export const stateToNode = (
         category_uuid: categories[0].uuid
       }
     ];
+
+    const ivrflowType = FlowTypes.VOICE;
+    if (state.flow.value.type !== ivrflowType) {
+      exits.push({
+        uuid: createUUID(),
+        destination_uuid: null
+      });
+      categories.push({
+        uuid: createUUID(),
+        name: StartFlowExitNames.Expired,
+        exit_uuid: exits[1].uuid
+      });
+      cases.push({
+        uuid: createUUID(),
+        arguments: ['expired'],
+        type: Operators.has_only_text,
+        category_uuid: categories[1].uuid
+      });
+    }
   }
 
   const actions = [];
